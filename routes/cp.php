b7cb40b34b7df9cb222af75b4922269557a30625
Venkatram Harish Belvadi <hello@vhbelvadi.com>
2026-02-05 09:58:59 +0000

    New route

--- routes/cp.php
+++ routes/cp.php
@@ -7,3 +7,6 @@ Route::name('og-generator.')->prefix('og-generator')->group(function () {
     Route::post('/settings', [SettingsController::class, 'update'])->name('update');
     Route::get('/preview', [SettingsController::class, 'preview'])->name('preview');
 });
+
+Route::statamic('og-generator', [OgGeneratorController::class, 'edit'])
+    ->name('og-generator.edit');
